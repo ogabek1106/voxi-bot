b3eb589976974e2b37526de8467c7a49980180fd
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2025-07-23 13:19:03 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Keep it in Railway ENV
+STORAGE_CHANNEL_ID = -1002714023986      # Your storage channel ID
+USER_FILE = "user_ids.json"              # Path to user file
+ADMIN_IDS = {1150875355}                 # Your Telegram user ID
