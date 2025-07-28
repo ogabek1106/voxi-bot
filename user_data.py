564f47071d42b8096f56e9ff6130433a533ee05d
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2025-07-28 12:26:35 +0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -1,27 +1,14 @@
 # user_data.py
+from tinydb import TinyDB, Query
 
-import json
-import os
-from config import USER_FILE
+db = TinyDB("user_data.json")
+users_table = db.table("users")
 
 def load_users():
-    if not os.path.exists(USER_FILE):
-        return None  # File not found
-    try:
-        with open(USER_FILE, "r") as f:
-            return set(json.load(f))
-    except json.JSONDecodeError:
-        return set()  # Corrupted file, fallback to empty set
+    return set(user["id"] for user in users_table.all())
 
-def save_users(user_ids):
-    with open(USER_FILE, "w") as f:
-        json.dump(list(user_ids), f)
-
-def add_user(user_ids, user_id):
-    if user_ids is None:
-        return False
-    if user_id not in user_ids:
-        user_ids.add(user_id)
-        save_users(user_ids)
-        return True
-    return False
+def add_user(user_ids_set, user_id):
+    if user_id not in user_ids_set:
+        users_table.insert({"id": user_id})
+        user_ids_set.add(user_id)
+    return True
