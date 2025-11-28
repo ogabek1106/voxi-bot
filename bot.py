4732125c663d5026cc2db836736e0f3268a33a3a
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2025-11-28 11:16:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,30 @@
-# bot.py 
+# bot.py
 import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import handlers
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Assalomualaykum!")
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", handlers.start_handler, pass_args=True))
+
+    # 🔥 NEW: user sends "3" → bot sends book 3
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handlers.numeric_message_handler))
 
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == "__main__":
     main()
