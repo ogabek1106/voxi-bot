0be6684bfa3268c0cd87beb9dccea809efc7d0c1
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-16 13:47:52 +0300

    Update books.py

--- books.py
+++ books.py
@@ -279,6 +279,11 @@ else:
             "file_id": "BQACAgIAAxkBAAEBjD5pt9nLL-ZWr-ywO_HFGBxXe2fdXwACRpoAAnxFwUkwr28pfDhTRjoE",
             "filename": "📘 Cambridge English Mindset for IELTS Student's Book 3.pdf + with Listening Audios 🎧",
             "caption": "📘 *Cambridge English Mindset for IELTS Student's Book 3.pdf*\n*+ with Listening Audios 🎧*\n\n⏰ File will be deleted in 15 minutes.\n\nMore 👉 @IELTSforeverybody"
+        },
+        "55": {
+            "file_id": "BQACAgIAAxkBAAEBjCZpt8u3gubh7bVJKqNYhJa9mr7cegACkpkAAnxFwUmatofKMpVGDDoE",
+            "filename": "📘 Cambridge English Mindset for IELTS Student's Book Foundation.pdf + with Listening Audios 🎧",
+            "caption": "📘 *Cambridge English Mindset for IELTS Student's Book Foundation.pdf*\n*+ with Listening Audios 🎧*\n\n⏰ File will be deleted in 15 minutes.\n\nMore 👉 @IELTSforeverybody"
         }
     }
 
